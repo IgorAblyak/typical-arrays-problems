2b18e82a6295c3545a69e266ba9d6ba25c795f43
IgorAblyak <iablyak@gmail.com>
2021-03-14 19:40:42 +0300

    feat: add functions minimal, maximal and average value of array

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 
 exports.min = function min (array) {
-  return 0;
+  let minNumber;
+  if (!array || array.length === 0) {
+    return 0;
+  } 
+  return Math.min.apply(null, array);
 }
 
 exports.max = function max (array) {
-  return 0;
+  let maxNumber;
+  if (!array || array.length === 0) {
+    return 0;
+  } 
+  return Math.max.apply(null, array);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  let sum = 0;
+  let avgNumber;
+  if (!array || array.length === 0) {
+    return 0;
+  } else {
+    for (let item of array) {
+      sum += item;
+    }
+    avgNumber = sum / array.length;
+  }
+  return avgNumber;
 }
